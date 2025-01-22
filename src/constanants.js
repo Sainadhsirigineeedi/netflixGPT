94c0a4270ebf09c8cd98d7984aedf14f63421d69
Sainadhsirigineedi <sainadhsirigineedi123@gmail.com>
2025-01-22 14:13:17 +0530

    tmdb changes

--- src/constanants.js
+++ src/constanants.js
@@ -1,7 +1,7 @@
 export const options={
     headers:{
        accept: 'application/json',
-       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjEwMGE1MjEwOWYyZTliZGFiNDdjOGJlOTA5ZmQ2MCIsIm5iZiI6MTczNDY5OTAzMC45MzYsInN1YiI6IjY3NjU2ODE2MGIyZmJiOWRlYTVkZjg5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.se55HCR4S7M3a5fggJuDKUy27djQ1o8yf7G3MoC4oVw',
+       Authorization: import.meta.env.VITE_TMDB_TOKEN,
       
     }
 }
